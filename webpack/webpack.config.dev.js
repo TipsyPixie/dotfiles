2b47c6e4387528bd9b1d5ed66e987957200c92ca
Seongjae Hwang <sjhwang@syncodax.com>
2018-07-27 16:00:34 +0900

    webpack, babel, flow added

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const rootPath = path.resolve(__dirname, '..');
+const srcPath = path.resolve(rootPath, 'src');
+const destPath = path.resolve(rootPath, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: `${srcPath}/index.jsx`,
+  context: rootPath,
+  output: {
+    path: `${destPath}/scripts`,
+    publicPath: `${destPath}/assets`,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['env', 'react'],
+          plugins: ['transform-class-properties']
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          'htmllint-loader',
+          'html-loader'
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  target: 'web',
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  bail: false,
+  watch: false,
+  optimization: {
+    minimize: false
+  }
+};
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const rootPath = path.resolve(__dirname, '..');
+const srcPath = path.resolve(rootPath, 'src');
+const destPath = path.resolve(rootPath, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: `${srcPath}/index.jsx`,
+  context: rootPath,
+  output: {
+    path: `${destPath}/scripts`,
+    publicPath: `${destPath}/assets`,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        enforce: 'pre',
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react'],
+          plugins: ['transform-class-properties']
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: '/node_modules/',
+        use: [
+          'htmllint-loader',
+          'html-loader'
+        ]
+      },
+      {
+        test: /\.sass$/,
+        exclude: '/node_modules/',
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  target: 'web',
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  bail: true,
+  watch: false,
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({})
+    ]
+  }
+};
